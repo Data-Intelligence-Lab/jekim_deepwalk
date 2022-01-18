cf568d1628dc2982a5645b4ef164a8461d96e03d
jekim <jekim9902@gmail.com>
2022-01-18 12:25:54 +0900

    220118_kmeans visualization

--- srcs/pca.py
+++ srcs/pca.py
@@ -12,9 +12,7 @@ def get_data():
 	df = pd.read_csv(filename, sep=' ', header=None)
 	node = df.iloc[:,0].to_numpy()
 	dim = df.iloc[:,1:] # pandas dataframe
-	# print(dim.shape)
 	std_dim = StandardScaler().fit_transform(dim) # numpy array
-	# print(np.shape(std_dim))
 	# print(std_dim.describe())
 	return [node, std_dim]
 
@@ -27,16 +25,46 @@ def do_pca(std_dim):
 	# print(pfdf.head())
 
 	# print(pca.explained_variance_ratio_)
+	# print(pca.explained_variance_ratio_[0] + pca.explained_variance_ratio_[1])
 	# print(sum(pca.explained_variance_ratio_))
 
 	return p_comp
 
-def print_graph(data):
+def print_pca_graph(data):
 	fig = plt.figure()
 	ax = plt.axes(projection='3d')
+
 	ax.scatter3D(data[:,0], data[:,1], data[:,2], color = "green")
-	plt.title("simple 3D scatter plot")
-	
+	plt.title("PCA results")
+	ax.set_xlabel("1st principal component")
+	ax.set_ylabel("2nd principal component")
+	ax.set_zlabel("3rd principal component")
+
+	# show plot
+	plt.show()
+
+def set_color(label):
+	if label == 0:
+		return 'r'
+	elif label == 1:
+		return 'g'
+	else:
+		return 'b'
+
+def print_kmeans_graph(result):
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+
+	# for color in ['r', 'g', 'b']
+	for i in range(0, 34):
+		color = set_color(result[i,3])
+		ax.scatter3D(result[i,0], result[i,1], result[i,2], color, depthshade=False)
+
+	plt.title("PCA results")
+	ax.set_xlabel("1st principal component")
+	ax.set_ylabel("2nd principal component")
+	ax.set_zlabel("3rd principal component")
+
 	# show plot
 	plt.show()
 
@@ -49,16 +77,16 @@ def main():
 	# print(type(kmeans.labels_))
 
 	result = np.copy(dim)
-	print(np.shape(result), np.shape(kmeans.labels_))
+	# print(np.shape(result), np.shape(kmeans.labels_))
 	values = np.array([kmeans.labels_]).transpose()
 	result = np.append(result, values, axis=1)
 	# result.append() = kmeans.labels_
 	# print(np.shape(dim), np.shape(kmeans.labels_))
-	print(result, values, np.shape(result))
-	print(np.array([kmeans.labels_]).transpose())
+	# print(np.shape(result))
+	# print(np.array([kmeans.labels_]).transpose())
 
-
-	# print_graph(dim)
+	print_kmeans_graph(result)
+	# print_pca_graph(dim)
 
 if __name__ == "__main__":
 	main()
